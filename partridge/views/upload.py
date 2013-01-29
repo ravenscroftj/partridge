9740c6f65f9f79ca4dc666cd4d61af9392bbfc62
James Ravenscroft <ravenscroftj@gmail.com>
2013-01-29 11:21:51 +0000

    Making improvements to upload view

--- partridge/views/upload.py
+++ partridge/views/upload.py
@@ -1,5 +1,7 @@
 """Upload form views for adding papers to the database"""
 
+from flask import render_template, request
+
 def upload():
     """Display the upload form allowing users to add their papers
     """
