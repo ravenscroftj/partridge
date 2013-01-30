a4e59069e5dd8b0de33d087975614ab870a9709c
James Ravenscroft <ravenscroftj@gmail.com>
2013-01-30 09:11:16 +0000

    Upload form now functional

--- partridge/views/upload.py
+++ partridge/views/upload.py
@@ -1,12 +1,33 @@
 """Upload form views for adding papers to the database"""
 
-from flask import render_template, request
+import os
+import uuid
+from flask import render_template, request, current_app
+from werkzeug import secure_filename
+
+
+ALLOWED_EXTENSIONS = ['.xml','.pdf']
 
 def upload():
     """Display the upload form allowing users to add their papers
     """
 
+
     if(request.method == "POST"):
-        print request.files
+
+        
+        destdir = current_app.config['PAPER_UPLOAD_DIR']
+
+        for f in request.files:
+
+            file = request.files[f]
+
+            name,ext = os.path.splitext(file.filename)
+
+            if ext in ALLOWED_EXTENSIONS:
+                fname = str(uuid.uuid4()) + ext
+                file.save(os.path.join(destdir,fname))
+
+
 
     return render_template("upload.html")
