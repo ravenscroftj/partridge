81afa01d6e7360c5313919c26d9f91dfbbb4673f
James Ravenscroft <ravenscroftj@gmail.com>
2013-01-30 10:51:43 +0000

    Started working on paper view - added url mapper

---
+++ partridge/views/paper.py
@@ -0,0 +1,9 @@
+"""View stats and info for a known paper in the database"""
+
+from flask import render_template, current_app
+
+def paper_profile(the_paper):
+    """Given a paper object, render a profile page""" 
+    
+    return ""
+
