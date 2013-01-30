5b010b39ad2cfcc72bd0b04655cf1c9071596bb4
James Ravenscroft <ravenscroftj@gmail.com>
2013-01-30 11:17:56 +0000

    title and author of papers now showing up

--- partridge/views/paper.py
+++ partridge/views/paper.py
@@ -5,5 +5,5 @@ from flask import render_template, current_app
 def paper_profile(the_paper):
     """Given a paper object, render a profile page""" 
     
-    return ""
+    return render_template("paperprofile.html", paper=the_paper)
 
