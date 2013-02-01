c24192965890deb7471760286329d38cb3905061
James Ravenscroft <ravenscroftj@gmail.com>
2013-02-01 14:50:15 +0000

    Fixed jquery firing twice thingy

--- partridge/static/js/queryform.js
+++ partridge/static/js/queryform.js
@@ -31,22 +31,43 @@ function showResults( getResult ){
 
 }
 
-/**
- * When everything is loaded, update the search results
- *
- */
-$(function(){
-    updateResults();
-});
-
 var constraints = 0;
+var constraintID = 1;
 
-/**
- * onClick handler for adding a new constrain to the query
- */
 $(function(){
-    $("#addConstraint").click(function(){
+    /**
+     * When everything is loaded, update the search results
+     *
+     */
+    updateResults();
 
+    /**
+     * onClick handler for adding a new constrain to the query
+     */
+    $("#addConstraint").click(function( event ){
         
+        var newHTML = "";
+
+        var section = $("#corescSelect").val();
+        var text = $("#queryText").val();
+
+        newHTML += "<li id=\"constraint" + constraintID + "\">" 
+        + text + " in " + section
+        + "<input type=\"hidden\" value=\"" + section + "\""
+        + " name=\"section\" />\n"
+        + "<input type=\"hidden\" value=\""+ text +"\"" 
+        + " name=\"query\" /><a href=\"#\">"
+        + "<i class=\"icon-remove-circle\"></i></li>";
+      
+
+        if( constraints == 0){
+            $("#queryList").html(newHTML);
+        }else{
+            $("#queryList").append(newHTML);
+        }
+
+        constraints++;
+        constraintID++;
+
     });
 });
