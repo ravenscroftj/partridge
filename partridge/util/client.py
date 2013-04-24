a7a9d3326959394fe8cf254979c47a4680145111
james <ravenscroftj@gmail.com>
2013-04-24 13:20:49 +0100

    Updated client address for laptop as master server

--- partridge/util/client.py
+++ partridge/util/client.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     QueueManager.register("get_work")
     QueueManager.register("return_result")
 
-    qm = QueueManager(address=("",1234), authkey="icecream")
+    qm = QueueManager(address=("ravey.zapto.org",1234), authkey="icecream")
     qm.connect()
 
 
