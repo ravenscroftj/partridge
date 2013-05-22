92ad8b58979916fdf3eb43d8dde1ff19165af35a
James Ravenscroft <ravenscroftj@gmail.com>
2013-05-22 14:05:03 +0100

    updated paper tree and forest with micro averaging

--- partridge/mltest/paper_forest.py
+++ partridge/mltest/paper_forest.py
@@ -15,6 +15,93 @@ from matplotlib import pyplot as plt
 from partridge.models import db
 from partridge.models.doc import Paper, PaperFile, Sentence, C_ABRV
 
+
+
+app = Flask(__name__)
+app.config.update(config)
+
+db.app = app
+db.init_app(app)
+
+TYPE_DIRS = {
+    "Review"     : "/home/james/dissertation/papers_for_type/review",
+    "Research"   : "/home/james/dissertation/papers_for_type/research",
+    "Case Study" : "/home/james/dissertation/papers_for_type/case", 
+    #"Subjective" : "/home/james/dissertation/papers_for_type/subjective",
+    "Essay"      : "/home/james/dissertation/papers_for_type/essay",
+    "Opinion"    : "/home/james/dissertation/papers_for_type/opinion",
+    "Perspective": "/home/james/dissertation/papers_for_type/perspective",
+    "Viewpoint"  : "/home/james/dissertation/papers_for_type/viewpoints",
+    "Synopsis"   : "/home/james/dissertation/papers_for_type/synopsis",
+    "Correspondence" : "/home/james/dissertation/papers_for_type/correspondence"
+}
+
+
+#set up data domain
+class_var = Orange.feature.Discrete("type")
+
+for type in TYPE_DIRS:
+    class_var.add_value(type)
+
+FEATURES = C_ABRV.keys()
+
+domain = Orange.data.Domain(
+        [Orange.feature.Continuous(x) for x in FEATURES], class_var)
+
+def build_papers_table():
+    """Given TYPE_DIRS, create an Orange Table using paper data from db"""
+
+    types = {}
+    for paper_type in TYPE_DIRS:
+        types[paper_type] = set(find_paper_ids(TYPE_DIRS[paper_type]))
+        
+        print "Found %d %s papers" % (len(types[paper_type]), paper_type)
+        
+
+    all_ids = set()
+
+    for type in types:
+        all_ids |= types[type]
+
+    print "Found %d papers in total" % len(all_ids)
+    
+    paper_table = Orange.data.Table(domain)
+    print "Loading Data..."
+
+    offset = 0
+    limit = 50
+    
+    q = Paper.query.filter(Paper.id.in_(all_ids))
+
+    while q.offset(offset).limit(limit).count() > 0:
+
+        print "Downloading batch of %d papers..." % limit
+
+        for paper in q.offset(offset).limit(limit).all():
+            if len(paper.sentences) < 1:
+                continue
+
+            inst_list = []
+            sentdist = paper.sentenceDistribution(True)
+            for coresc in FEATURES:
+                inst_list.append( sentdist[coresc] * 100 / len(paper.sentences) )
+
+            for type in types:
+                if paper.id in types[type]:
+                    inst_list.append(type)
+                    break
+                
+            inst = Orange.data.Instance(domain, inst_list)
+            paper_table.append(inst)
+
+            del paper
+
+        offset += limit
+
+    #return the table to the caller
+    return paper_table
+
+
 def find_paper_ids( dir ):
 
     ids = []
@@ -68,10 +155,21 @@ def printMeasures( confusion ):
             fm     = 0
 
         print "%s\t\t%f\t\t%f\t\t%f" % ( c, recall, prec, fm)
+
+
+#---------------------------------------------------------------
     
 def printResults(tabledata, tree, classes):
+    confusion = buildConfusion(tabledata, tree, classes)
+    printConfusion( confusion, classes )
+    printMeasures( confusion )
+
+#---------------------------------------------------------------
+
+def buildConfusion(tabledata, tree, classes):
+    
     confusion = {}
-    # Set up empty confusion matrix 
+    
     for c1 in classes:
         confusion[c1] = {}
         for c2 in classes:
@@ -82,8 +180,7 @@ def printResults(tabledata, tree, classes):
         predclass    = tree(d)
         confusion[str(correctclass)][str(predclass)] += 1
 
-    printConfusion( confusion, classes )
-    printMeasures( confusion )
+    return confusion
 
 
 class SimpleTreeLearnerSetProb():
@@ -102,143 +199,73 @@ class SimpleTreeLearnerSetProb():
 #----------------------------------------------------------
 
 
-app = Flask(__name__)
-app.config.update(config)
-
-db.app = app
-db.init_app(app)
-
-TYPE_DIRS = {
-    "Review"     : "/home/james/dissertation/papers_for_type/review",
-    "Research"   : "/home/james/dissertation/papers_for_type/research",
-    "Case Study" : "/home/james/dissertation/papers_for_type/case", 
-    "Essay"      : "/home/james/dissertation/papers_for_type/essay",
-    "Opinion"    : "/home/james/dissertation/papers_for_type/opinion",
-    "Perspective": "/home/james/dissertation/papers_for_type/perspective",
-    "Viewpoint"  : "/home/james/dissertation/papers_for_type/viewpoints",
-    "Synopsis"   : "/home/james/dissertation/papers_for_type/synopsis",
-    "Correspondence" : "/home/james/dissertation/papers_for_type/correspondence"
-}
-
-types = {}
-for paper_type in TYPE_DIRS:
-    types[paper_type] = set(find_paper_ids(TYPE_DIRS[paper_type]))
-    
-    print "Found %d %s papers" % (len(types[paper_type]), paper_type)
-    
-
-all_ids = set()
-
-for type in types:
-    all_ids |= types[type]
-
-print "Found %d papers in total" % len(all_ids)
-
-#set up data domain
-class_var = Orange.feature.Discrete("type")
-
-for type in types:
-    class_var.add_value(type)
-
-FEATURES = C_ABRV.keys()
-
-domain = Orange.data.Domain([Orange.feature.Continuous(x) for x in FEATURES], 
-class_var)
-
-paper_table = Orange.data.Table(domain)
-print "Loading Data..."
-
-offset = 0
-limit = 50
-
-q = Paper.query.filter(Paper.id.in_(all_ids))
-
-while q.offset(offset).limit(limit).count() > 0:
-
-    print "Downloading batch of %d papers..." % limit
-
-    for paper in q.offset(offset).limit(limit).all():
-        if len(paper.sentences) < 1:
-            continue
-
-        inst_list = []
-        sentdist = paper.sentenceDistribution(True)
-        for coresc in FEATURES:
-            inst_list.append( sentdist[coresc] * 100 / len(paper.sentences) )
-
-        for type in types:
-            if paper.id in types[type]:
-                inst_list.append(type)
-                break
-            
-        inst = Orange.data.Instance(domain, inst_list)
-        paper_table.append(inst)
-
-        del paper
-
-    offset += limit
+def main():
 
 
+    paper_table = build_papers_table()
 
-tree = Orange.classification.tree.TreeLearner(min_instances=5,
-measure="gainRatio", rand=random)
-rf_def = Orange.ensemble.forest.RandomForestLearner(trees=50,
-base_learner=tree, name="for_gain", rand=random)
+    tree = Orange.classification.tree.TreeLearner(min_instances=5,
+    measure="gainRatio", rand=random)
+    rf_def = Orange.ensemble.forest.RandomForestLearner(trees=50,
+    base_learner=tree, name="for_gain", rand=random)
 
-#random forests with simple trees - simple trees do random attribute selection by themselves
-st = Orange.classification.tree.SimpleTreeLearner(min_instances=5)
-stp = SimpleTreeLearnerSetProb(st)
-rf_simple = Orange.ensemble.forest.RandomForestLearner(learner=stp, trees=50, name="for_simp")
+    #random forests with simple trees - simple trees do random attribute selection by themselves
+    st = Orange.classification.tree.SimpleTreeLearner(min_instances=5)
+    stp = SimpleTreeLearnerSetProb(st)
+    rf_simple = Orange.ensemble.forest.RandomForestLearner(learner=stp, trees=50, name="for_simp")
 
-learners = [ rf_def, rf_simple ]
-
-
-results = Orange.evaluation.testing.proportion_test([rf_def,rf_simple], paper_table, times=1)
-points = Orange.evaluation.scoring.compute_ROC(results)[0]
-
-xs = [ p[0] for p in points ]
-ys = [ p[1] for p in points ]
-
-plt.xlim(xmax=1)
-plt.ylabel("True Positives")
-plt.xlabel("False Positives")
-for x,y in [p for p in points]:
-    plt.plot([0,1],[0,1],'k--')
-    plt.plot(xs,ys,'b-')
-
-plt.savefig("ROC.png")
-
-print "--------------------3 Fold Cross Validation------------------------"
-
-results = Orange.evaluation.testing.cross_validation(learners, paper_table,
-        folds=3, storeClassifiers=1,
-        random_generator=Orange.misc.Random(int(random.random()*10)))
+    learners = [ rf_def, rf_simple ]
 
 
-import cPickle
+    results = Orange.evaluation.testing.proportion_test([rf_def,rf_simple], paper_table, times=1)
+    points = Orange.evaluation.scoring.compute_ROC(results)[0]
+
+    xs = [ p[0] for p in points ]
+    ys = [ p[1] for p in points ]
+
+    plt.xlim(xmax=1)
+    plt.ylabel("True Positives")
+    plt.xlabel("False Positives")
+    for x,y in [p for p in points]:
+        plt.plot([0,1],[0,1],'k--')
+        plt.plot(xs,ys,'b-')
+
+    plt.savefig("ROC.png")
+
+    print "--------------------3 Fold Cross Validation------------------------"
+
+    results = Orange.evaluation.testing.cross_validation(learners, paper_table,
+            folds=3, storeClassifiers=1,
+            random_generator=Orange.misc.Random(int(random.random()*10)))
 
 
-print "Learner  CA     Brier  AUC"
-for i in range(len(learners)):
-    print "%-8s %5.3f  %5.3f  %5.3f" % (learners[i].name, \
-    Orange.evaluation.scoring.CA(results)[i], 
-    Orange.evaluation.scoring.Brier_score(results)[i],
-    Orange.evaluation.scoring.AUC(results)[i])
+    import cPickle
 
 
-    with open(("results_%d.pickle" % i), "wb") as f:
-        cPickle.dump(results, f)
+    print "Learner  CA     Brier  AUC"
+    for i in range(len(learners)):
+        print "%-8s %5.3f  %5.3f  %5.3f" % (learners[i].name, \
+        Orange.evaluation.scoring.CA(results)[i], 
+        Orange.evaluation.scoring.Brier_score(results)[i],
+        Orange.evaluation.scoring.AUC(results)[i])
 
 
-    for k in range(0,3):
-        indices = [paper_table[x] for x in range(0,len(paper_table)) 
-            if results.results[x].iteration_number == k]
-        printResults(indices, results.classifiers[k][i], types.keys())
-
-print "Storing tree learned from data"
+        with open(("results_%d.pickle" % i), "wb") as f:
+            cPickle.dump(results, f)
 
 
-tree = rf_simple( paper_table)
+        for k in range(0,3):
+            indices = [paper_table[x] for x in range(0,len(paper_table)) 
+                if results.results[x].iteration_number == k]
+            printResults(indices, results.classifiers[k][i], TYPE_DIRS.keys())
 
-with open("paper_types.model",'wb') as f:
-    cPickle.dump(tree, f)
+    print "Storing tree learned from data"
+
+
+    tree = rf_simple( paper_table)
+
+    with open("paper_types.model",'wb') as f:
+        cPickle.dump(tree, f)
+
+if __name__== "__main__":
+    main()
--- partridge/mltest/paper_tree.py
+++ partridge/mltest/paper_tree.py
@@ -1,3 +1,63 @@
 """ Like paper forest but trains single Dtree so we can see decisions
 
 """
+
+import os
+import Orange
+import orngTree
+
+from partridge.mltest.paper_forest import FEATURES, TYPE_DIRS,\
+build_papers_table, printResults, buildConfusion, printConfusion, printMeasures
+
+def buildTotalConfusions(confusions, keys):
+    
+    confusion = {y : { x:0 for x in keys } for y in keys}
+
+
+    for c in confusions:
+        for c1,c2 in [(c1,c2) for c1 in keys for c2 in keys]:
+            confusion[c1][c2] += c[c1][c2]
+
+    return confusion
+            
+        
+
+def main():
+    """Main script"""
+
+    paper_table = build_papers_table()
+    tree = orngTree.TreeLearner(minSubset=5,sameMajorityPruning=True)
+
+    learners = [tree]
+
+    FOLDS = 10
+
+    results = Orange.evaluation.testing.cross_validation(learners, paper_table,
+    folds=FOLDS,storeClassifiers=1)
+
+    confusions = []
+
+    print "Learner  CA     Brier  AUC"
+    for i in range(len(learners)):
+        print "%-8s %5.3f  %5.3f  %5.3f" % (learners[i].name, \
+        Orange.evaluation.scoring.CA(results)[i], 
+        Orange.evaluation.scoring.Brier_score(results)[i],
+        Orange.evaluation.scoring.AUC(results)[i])
+    
+        for k in range(0,FOLDS):
+            indices = [paper_table[x] for x in range(0,len(paper_table)) 
+                if results.results[x].iteration_number == k]
+            
+            confusions.append(buildConfusion(indices,
+            results.classifiers[k][i], TYPE_DIRS.keys()))
+
+        confusion = buildTotalConfusions(confusions, TYPE_DIRS.keys())
+
+        printConfusion(confusion, TYPE_DIRS.keys())
+        printMeasures(confusion)
+        
+
+        orngTree.printTxt(results.classifiers[k][i], leafFields=['major', 'contingency'])
+
+if __name__ == "__main__":
+    main()
