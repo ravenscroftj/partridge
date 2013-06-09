9c7e5b09e64c9ca319b5047ff25c5b079213c93d
James Ravenscroft <ravenscroftj@gmail.com>
2013-06-09 07:56:37 +0100

    Added twitter as a dependency of setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
             'progressbar>=2.3',
             'pyinotify>=0.9.4',
             'alembic>=0.4.2',
-            'Orange>=2.6'],
+            'Orange>=2.6',
+            'twitter>=1.9.4'],
 
     #program entrypoints
     entry_points ={
