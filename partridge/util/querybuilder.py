49451c6f498cdcbe1d26932f27299c22ecd609dd
James Ravenscroft <ravenscroftj@gmail.com>
2013-06-12 11:55:07 +0100

    Dun fixed querybuilder

--- partridge/util/querybuilder.py
+++ partridge/util/querybuilder.py
@@ -2,40 +2,45 @@
 
 from sqlalchemy import func, or_, and_
 from partridge.models.doc import Paper,Sentence,Author, C_ABRV, PAPER_TYPES
+from partridge.models import db
 
 class PaperQueryBuilder:
-
+   
     paper_q = None
     clauses = []
     offset  = 0
 
-    def build_query(self, *args, **constraints):
+    def build_query(self, **constraints):
         
         self.paper_q = Paper.query
+        self.clauses = []
 
         for key in constraints:
             value = constraints[key]
             attr = key.split("_")[0]
 
             if(attr == "any"):
-                self.paper_q = self.paper_q.join("sentences")
-                self.clauses.append( Sentence.text.like("%%%s%%" % value) )
-
-            if(attr == "offset"):
+                self.clauses.append( Paper.id.in_(
+                        db.session.query(Sentence.paper_id).filter(Sentence.text.like("%%%s%%" % value) ).subquery()
+                ))
+   
+            elif(attr == "offset"):
                 offset = value
-
-            if(attr == "papertype"):
+   
+            elif(attr == "papertype"):
                 if value != "":
                     self.clauses.append( Paper.type == value )
-
-            if(attr == "author"):
+   
+            elif(attr == "author"):
                 self.paper_q = self.paper_q.join("authors")
                 self.clauses.append( Author.surname.like("%%%s%%" % value))
-
-            if attr in C_ABRV.keys():
-                self.paper_q = self.paper_q.join("sentences")
-                self.clauses.append( ( Sentence.coresc == attr ) & 
-                    Sentence.text.like("%%%s%%" % value))
+   
+            elif attr in C_ABRV.keys():
+                self.clauses.append(Paper.id.in_(
+                        db.session.query(Sentence.paper_id).filter(
+                                (Sentence.text.like("%%%s%%" % value)) &
+                                (Sentence.coresc == attr)).subquery()
+                )) 
 
     def get_query(self):
-        return self.paper_q.filter(and_(*self.clauses)).group_by(Paper.id, Paper.doi, Paper.title, Paper.abstract, Paper.type)
+        return self.paper_q.filter(and_(*self.clauses))
